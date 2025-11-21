3efecf117896944d84dcaacf316e5ead6de72297
wasabi315 <wasabi.nvim.st@gmail.com>
2025-11-21 10:00:04 +0900

    Refactor extractor

--- agda-signature-extractor/app/Main.hs
+++ agda-signature-extractor/app/Main.hs
@@ -10,6 +10,7 @@ import Agda.Interaction.Options.Warnings qualified as Agda
 import Agda.Syntax.Abstract.Name qualified as Abstract
 import Agda.Syntax.Common.Pretty qualified as Pretty
 import Agda.Syntax.Concrete qualified as Concrete
+import Agda.Syntax.Position qualified as Agda
 import Agda.Syntax.Translation.AbstractToConcrete qualified as Agda
 import Agda.Syntax.Translation.InternalToAbstract qualified as Agda
 import Agda.Utils.FileName qualified as Agda
@@ -30,11 +31,15 @@ import System.IO
 --------------------------------------------------------------------------------
 -- Extract summary information about definitions from Agda library
 
-loadLibraryFileContent :: FilePath -> Agda.TCM ([FilePath], Agda.OptionsPragma)
-loadLibraryFileContent currentDirectory = do
-  [libraryFile] <- Agda.libToTCM $ Agda.mkLibM [] $ Agda.getAgdaLibFiles' currentDirectory
-  pure (libraryFile._libIncludes, libraryFile._libPragmas)
+-- | Load the library file from the given root directory.
+loadLibraryFile :: FilePath -> Agda.TCM Agda.AgdaLibFile
+loadLibraryFile libraryRootDirectory = do
+  libraryFiles <- Agda.libToTCM $ Agda.mkLibM [] $ Agda.getAgdaLibFiles' libraryRootDirectory
+  case libraryFiles of
+    [libraryFile] -> pure libraryFile
+    _ -> throwError $ Agda.Exception Agda.noRange "Expected exactly one library file"
 
+-- | Find the paths to all Agda modules in the given include paths.
 findAllModulePath :: [FilePath] -> IO [FilePath]
 findAllModulePath includePaths = do
   sort . map Find.infoPath . concat <$> forM includePaths \includePath -> do
@@ -48,6 +53,7 @@ findAllModulePath includePaths = do
       []
       includePath
 
+-- | Type check the given module and return the interface.
 typeCheckModule :: FilePath -> Agda.TCM Agda.Interface
 typeCheckModule moduleFilePath = do
   let agdaOptions =
@@ -65,14 +71,7 @@ typeCheckModule moduleFilePath = do
   typeCheckResult <- Agda.typeCheckMain Agda.TypeCheck source
   pure $ Agda.crInterface typeCheckResult
 
-data DefinitionKind
-  = Axiom
-  | Function
-  | Datatype
-  | Constructor
-  | Record
-  | Primitive
-
+-- | A summary of a definition in the interface.
 data DefinitionSummary = DefinitionSummary
   { name :: Abstract.Name,
     topLevelModuleName :: Concrete.QName,
@@ -82,6 +81,16 @@ data DefinitionSummary = DefinitionSummary
     signature :: Concrete.Expr
   }
 
+data DefinitionKind
+  = Axiom
+  | Function
+  | Datatype
+  | Constructor
+  | Record
+  | Primitive
+
+-- | Convert the given definition to a definition kind.
+-- Returns @Nothing@ for uninteresting definitions.
 definitionKind :: Agda.Defn -> Maybe DefinitionKind
 definitionKind = \case
   Agda.DatatypeDefn {} -> Just Datatype
@@ -98,6 +107,7 @@ definitionKind = \case
     | otherwise -> Just Function
   _ -> Nothing
 
+-- | Summarize the given @Agda.Interface@ by extracting the definitions and converting them to @[DefinitionSummary]@.
 summarizeInterface :: Agda.Interface -> Agda.TCM [DefinitionSummary]
 summarizeInterface interface = do
   let topLevelModuleName = Agda.mnameToConcrete interface.iModuleName
@@ -107,8 +117,7 @@ summarizeInterface interface = do
         case definitionKind definition.theDef of
           Nothing -> pure mempty
           Just kind -> do
-            signature <- Agda.reify definition.defType
-            signature' <- Agda.abstractToConcrete_ signature
+            signature <- Agda.abstractToConcrete_ =<< Agda.reify definition.defType
             let qname = definition.defName
                 summary =
                   DefinitionSummary
@@ -117,7 +126,7 @@ summarizeInterface interface = do
                       moduleName = Agda.mnameToConcrete qname.qnameModule,
                       kind,
                       qualifiedName = Agda.qnameToConcrete qname,
-                      signature = signature'
+                      signature
                     }
             pure $ Endo (summary :)
 
@@ -126,6 +135,7 @@ summarizeInterface interface = do
 --------------------------------------------------------------------------------
 -- Output the summaries as JSONL documents
 
+-- | A document for a definition summary.
 data Document = Document
   { pageContent :: String,
     metadata :: Metadata
@@ -148,10 +158,20 @@ instance Json.ToJSON Metadata where
 instance Json.ToJSON Document where
   toEncoding = Json.genericToEncoding Json.defaultOptions {Json.fieldLabelModifier = Json.camelTo2 '_'}
 
+-- | Write the given list of values to the given handle as JSONL.
+hPutJsonL :: (Json.ToJSON a) => Handle -> [a] -> IO ()
+hPutJsonL handle = traverse_ \value -> do
+  ByteString.hPutStr handle $ Json.encode value
+  ByteString.hPutStr handle "\n"
+
+-- | Render the given document in one line.
+renderOneLine :: Pretty.Doc -> String
+renderOneLine = Pretty.renderStyle Pretty.style {Pretty.mode = Pretty.OneLineMode}
+
+-- | Convert the given definition summary to a document.
 formatDefinitionSummary :: DefinitionSummary -> Document
 formatDefinitionSummary summary = Document {..}
   where
-    renderOneLine = Pretty.renderStyle Pretty.style {Pretty.mode = Pretty.OneLineMode}
     metadata = Metadata {..}
     pageContent = renderOneLine $ Pretty.pretty summary.name <> " : " <> Pretty.pretty summary.signature
     name = renderOneLine $ Pretty.pretty summary.name
@@ -168,48 +188,64 @@ formatDefinitionSummary summary = Document {..}
     signature = renderOneLine $ Pretty.pretty summary.signature
 
 --------------------------------------------------------------------------------
--- Command line options
+-- Command line arguments
 
-newtype ExtractorOptions = ExtractorOptions
-  { outDirectory :: FilePath
+data ExtractArguments = ExtractArguments
+  { -- | The current working directory of the extractor.
+    currentDirectory :: FilePath,
+    -- | The relative directory to the Agda library.
+    relativeLibraryDirectory :: FilePath,
+    -- | The relative directory to write the signatures to.
+    relativeOutDirectory :: FilePath
   }
 
-optionParser :: Opt.Parser ExtractorOptions
-optionParser =
-  ExtractorOptions
-    <$> Opt.strOption (Opt.long "outDir" <> Opt.metavar "OUT_DIR" <> Opt.help "The directory to write the signatures to")
+parseArguments :: IO ExtractArguments
+parseArguments = do
+  currentDirectory <- getCurrentDirectory
 
-optionParserInfo :: Opt.ParserInfo ExtractorOptions
-optionParserInfo =
-  Opt.info (optionParser Opt.<**> Opt.helper) (Opt.fullDesc <> Opt.progDesc "Extract signatures from Agda library")
+  let optionParser =
+        ExtractArguments currentDirectory
+          <$> Opt.strOption (Opt.long "libDir" <> Opt.metavar "LIB_DIR" <> Opt.help "The *relative* directory to the Agda library. Defaults to the current directory" <> Opt.value "./")
+          <*> Opt.strOption (Opt.long "outDir" <> Opt.metavar "OUT_DIR" <> Opt.help "The *relative* directory to write the signatures to")
+
+      parserInfo = Opt.info (optionParser Opt.<**> Opt.helper) (Opt.fullDesc <> Opt.progDesc "Extract signatures from Agda library")
+
+  Opt.execParser parserInfo
+
+libraryDirectory :: ExtractArguments -> FilePath
+libraryDirectory arguments =
+  arguments.currentDirectory </> arguments.relativeLibraryDirectory
+
+summaryFilePath :: ExtractArguments -> (Abstract.ModuleName -> FilePath)
+summaryFilePath arguments = \moduleName ->
+  outDirectory
+    </> foldr ((</>) . show . Pretty.pretty) "" moduleName.mnameToList
+      <.> "jsonl"
+  where
+    outDirectory = arguments.currentDirectory </> arguments.relativeOutDirectory
 
 --------------------------------------------------------------------------------
 
+withFileEnsuringDirectory :: FilePath -> IOMode -> (Handle -> IO a) -> IO a
+withFileEnsuringDirectory filePath mode action = do
+  createDirectoryIfMissing True (takeDirectory filePath)
+  withFile filePath mode action
+
 main :: IO ()
 main = do
-  currentDirectory <- getCurrentDirectory
-  extractorOptions <- Opt.execParser optionParserInfo
-
-  let outDirectory = currentDirectory </> extractorOptions.outDirectory
+  arguments <- parseArguments
 
   Agda.runTCMTop' do
-    (includePaths, libOpts) <- loadLibraryFileContent currentDirectory
+    libraryFile <- loadLibraryFile $ libraryDirectory arguments
 
-    Agda.checkAndSetOptionsFromPragma libOpts
+    Agda.checkAndSetOptionsFromPragma libraryFile._libPragmas
 
-    moduleFilePaths <- liftIO $ findAllModulePath includePaths
+    moduleFilePaths <- liftIO $ findAllModulePath libraryFile._libIncludes
 
-    for_ moduleFilePaths \moduleFilePath -> flip catchError (const $ pure ()) do
+    let getSummaryFilePath = summaryFilePath arguments
+    for_ moduleFilePaths \moduleFilePath -> flip catchError (\_ -> pure ()) do
       interface <- typeCheckModule moduleFilePath
       summaries <- summarizeInterface interface
-      let moduleName = interface.iModuleName
-          summariesFilePath =
-            outDirectory
-              </> foldr ((</>) . show . Pretty.pretty) "" moduleName.mnameToList
-                <.> "jsonl"
-      liftIO do
-        createDirectoryIfMissing True (takeDirectory summariesFilePath)
-        withFile summariesFilePath WriteMode \handle -> do
-          for_ summaries \summary -> do
-            ByteString.hPutStr handle $ Json.encode $ formatDefinitionSummary summary
-            ByteString.hPutStr handle "\n"
+      let path = getSummaryFilePath interface.iModuleName
+      liftIO $ withFileEnsuringDirectory path WriteMode \handle ->
+        hPutJsonL handle $ map formatDefinitionSummary summaries
